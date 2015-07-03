cec251d0717bb4189d409194f679c9fe6ba66542
lammoth <fjhidalgo@wtelecom.es>
2015-07-03 08:58:00 +0200

    Plugins live directives implemented

--- client/components/plugin/plugin.directive.js
+++ client/components/plugin/plugin.directive.js
@@ -1,19 +1,22 @@
 'use strict';
 
 angular.module('thedashboardApp')
-  .directive('acquisitorGraphicOptions', function (Plugin) {
-      return {
-        templateUrl: function() {
-          var acquisitorPlugin = null;
-          var acquisitorPluginPromise = Plugin.broker('getAcquisitor');
-          acquisitorPluginPromise.then(function(acquisitorPlugin) {
-            console.log(acquisitorPlugin);
-            return 'components/plugin/' + acquisitorPlugin + '/acquisitor-graphic-options.html';
+  .directive('acquisitorGraphicOptions', function ($compile, $templateRequest, Plugin) {
+    return {
+      restrict: 'E',
+      link: function (scope, element, attrs) {
+        // Request to get the acquisitor plugin active 
+        var acquisitorPluginPromise = Plugin.broker('getAcquisitor');
+ 
+        acquisitorPluginPromise.then(function(acquisitorPlugin) {
+          var templateUrl = 'components/plugin/' + acquisitorPlugin + '/acquisitor-graphic-options.html';
+
+          $templateRequest(templateUrl).then(function(html){
+            element.append($compile(html)(scope));
+            $compile(element.contents())(scope);
           });
-        },
-        restrict: 'EA',
-        link: function (scope, element, attrs) {
-        }
-      };
-    
+
+        });
+      }
+    };
   });
