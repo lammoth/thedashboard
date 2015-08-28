a966889589066ea961da3b7f8cf85aadce2c7c16
Marcos García <marcosluisgm@gmail.com>
2015-08-28 12:45:10 +0200

    Added user profile dropdown with logout function

--- client/app/main/menu.controller.js
+++ client/app/main/menu.controller.js
@@ -1,10 +1,16 @@
 'use strict';
 
 angular.module('thedashboardApp')
-  .controller('MenuController', function ($scope) {
+  .controller('MenuController', function ($scope, Auth, $location) {
     // Initialize metisMenu plugin to sidebar menu
     $('#side-menu').metisMenu();
 
     // Set minimal height of #wrapper to fit the window
     fixWrapperHeight();
+
+
+    $scope.logout = function() {
+      Auth.logout();
+      $location.path('/login');
+    };
   });
